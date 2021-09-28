626f5fb13bccf8777e3ee01ae1e8e4be0a7fbae9
My PC <My PC@DESKTOP-IBQBT32>
2021-09-28 15:32:01 +0700

    6

--- BT4/Models/Person.cs
+++ BT4/Models/Person.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace BT4.Models
 {
     public class Person
     {
         public string PersonID { get; set; }
+        [AllowHtml]
         public string PersonName { get; set; }
     }
 }
