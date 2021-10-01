51904e2b0060149e98b76190b677200c9b25006a
My PC <My PC@DESKTOP-IBQBT32>
2021-10-01 16:39:05 +0700

    7

---
+++ BT4/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using LapTrinhQuanLyDb.Models;
+
+namespace LapTrinhQuanLyDb.Controllers
+{
+    public class AccontController : Controller
+    {
+        Encrytion encry = new Encrytion();
+        LTQLDbContext db = new LTQLDbContext();
+        // GET: Accont
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Register(Account acc)
+        {
+            if (ModelState.IsValid)
+            {
+                //mã hóa mật khẩu trước khi lưu vào database
+                acc.Password = encry.PasswordEncrytion(acc.Password);
+                db.Accounts.Add(acc);
+                db.SaveChanges();
+                return RedirectToAction("Login", " Account");
+            }
+            return View(acc);
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Login(Account acc)
+        {
+            if (ModelState.IsValid)
+            {
+                string encrytionpass = encry.PasswordEncrytion(acc.Password);
+                var model = db.Accounts.Where(m => m.UserName == acc.UserName && m.Password == encrytionpass).ToList().Count();
+                //thông tin đăng nhập chính xác
+                if (model == 1)
+                {
+                    FormsAuthentication.SetAuthCookie(acc.UserName, true);
+                    return RedirectToAction("Index", " Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "THông tin đăng nhập không chính xác ");
+                }
+            }
+            return View(acc);
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
